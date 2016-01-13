7089e2abf5392346a8fc4c34f736ed4dcaa02a78
Jonathan Kilgore <jonathan.kilgore58@gmail.com>
2016-01-13 16:08:12 -0600

    complted steps 1 and 2

--- Tweeterizzer/ViewController.m
+++ Tweeterizzer/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextViewDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *counting;
 
 @end
 
@@ -21,23 +23,49 @@
     NSLog(@"%@", self.textView.text);
     self.textView.text = @"";
     
-}
-
-- (IBAction)twitterButton:(UIButton *)sender {
-    NSString *enteredText = self.textView.text;
-    NSInteger length = enteredText.length;
-//    NSMutableString *vowels = [NSMutableString new];
-//    
-//    NSLog(@"entered text = %lu", length);
-
-//    for (int i = enteredText.length; i >= 0; i++) {
-//        <#statements#>
- //   }
+    
+    
+//    NSString *string = self.textView.text;
+//    _block NSUInteger count = 0;
+//    [string enumerateSubstringsInRange:NSMakeRange(0, [string length])
+//                               options:NSStringEnumerationByComposedCharacterSequences
+//                            usingBlock:^(NSString *subString, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+//                                count++;
+//                            }];
+//    NSLog(@"%ld %ld", (long) count, (long)[string length]);
     
 }
 
+- (IBAction)twitterButton:(UIButton *)sender {
+  //  NSString *enteredText = self.textView.text;
+    //NSInteger length = enteredText.length;
+    
+    NSMutableString *stringa = [[NSMutableString alloc] initWithFormat:@"%@", self.textView.text];
+    
+    if ([stringa length] < 3) {
+        [stringa appendString:@"x"];
+    }
+    
+    NSMutableString *consonanti = [[NSMutableString alloc] init];
+    
+    NSCharacterSet *vocali = [NSCharacterSet characterSetWithCharactersInString:@"aeiouàèìòùáéíóúAEIOUÀÈÌÒÙÁÉÍÓÚ"];
+    
+    NSRange r;
+    
+    for (int i=0; i < [stringa length]; i++) {
+        
+        r = [stringa rangeOfCharacterFromSet:vocali];
+        
+        if (![vocali characterIsMember:[stringa characterAtIndex:i]]) {
+            [consonanti appendFormat:@"%C",[stringa characterAtIndex:i]];
+        }
+        else {
+        }
+    }
+    self.textView.text = consonanti;
 
-
+    
+}
 
 
 @end
