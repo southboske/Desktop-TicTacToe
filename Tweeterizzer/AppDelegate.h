13b17a53904886ba14be8661c1b56765cc0db77f
south boske <southboske@souths-macbook-pro.local>
2016-01-13 13:46:21 -0600

    Initial Commit

---
+++ Tweeterizzer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tweeterizzer
+//
+//  Created by south boske on 1/13/16.
+//  Copyright © 2016 south boske. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tweeterizzer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tweeterizzer
+//
+//  Created by south boske on 1/13/16.
+//  Copyright © 2016 south boske. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
